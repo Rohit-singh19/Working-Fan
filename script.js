1b636aa9b274bcf8e5f203ee6cfd3b8053d12016
Rohit singh <77694147+Rohit-singh19@users.noreply.github.com>
2021-06-06 20:03:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,28 @@ const fastFan = document.getElementById('fanfast');
 const onBtn = document.getElementById('fanon');
 const fastBtn = document.getElementById('fanfast');
 
+let x = undefined;
+
 fanOnBtn.addEventListener('click', () => {
     fanBlades.classList.remove('fast-animate')
     fanBlades.classList.toggle('animate');
     onBtn.classList.toggle('active');
     fastBtn.classList.remove('active');
+    if (x == undefined || x == false) {
+        x = true;
+    }
+    else if (x == true) {
+        x = false;
+    }
 });
 
-fastFan.addEventListener('click', () => {
-    fanBlades.classList.toggle('fast-animate');
-    fastBtn.classList.toggle('active');
+fastFan.addEventListener('click', (e) => {
+    if (x == true) {
+        fanBlades.classList.toggle('fast-animate');
+        fastBtn.classList.toggle('active');
+    }
+    else{
+        
+        e.preventDefault();
+    }
 });
